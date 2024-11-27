8263bdd5ef00ccdde56869eb5597f63b1deaca0d
noahm06304 <117481568+noahm06304@users.noreply.github.com>
2024-11-27 13:12:23 -0600

    Update DatabaseFunctions.py

--- DatabaseFunctions.py
+++ DatabaseFunctions.py
@@ -20,37 +20,61 @@ def showAllStudents():
         students = cursor.fetchall()
 
         #Header
-        #printPage("StudentRecord.txt")
-        print(f"{'ID':<8} {'Name':<15} {'Phone':<15} {'Major':<5}")
+        printPage("StudentRecord.txt")
+        print(f"{'ID':<10} {'Name':<16} {'Phone':<15} {'Major':<5}")
+
+        #Iterate through every student
         for student in students:
-            print(student) #Note: needs formatting so it doesn't just show the raw data
+
+            #Unpack student into multiple variables
+            stud_id, name, age, gender, major, phone = student
+
+            #Display the relevant student information
+            print(f"{stud_id:<10} {name:<16} {phone:<15} {major:<5}")
         print()
 
 def showStudentName(name):
     with sqlite3.connect("school.db") as conn:
         cursor = conn.cursor()
 
-        #Displays a student based on their name
+        #Selects all students with a certain name
         cursor.execute("SELECT * FROM Student WHERE name = ?", (name,))
 
-        printPage("StudentRecord.txt")
         students = cursor.fetchall()
+        
+        #Header
+        printPage("StudentRecord.txt")
+        print(f"{'ID':<10} {'Name':<16} {'Phone':<15} {'Major':<5}")
 
         if students:
+            
+            #Iterate through students
             for student in students:
-                print(i) #Note: needs formatting so it doesn't just show the raw data
-            print()
+
+                #Unpack student into multipel variables
+                stud_id, name, age, gender, major, phone = student
+
+            #Display the relevant student information
+            print(f"{stud_id:<10} {name:<16} {phone:<15} {major:<5}")
 
 def showStudentID(ID):
     with sqlite3.connect("school.db") as conn:
         cursor = conn.cursor()
 
-        #Displays a student based on their ID
+        #Selects a student based on their ID
         cursor.execute("SELECT * FROM Student WHERE id = ?", (ID,))
 
+        #Header
         printPage("StudentRecord.txt")
+        print(f"{'ID':<10} {'Name':<16} {'Phone':<15} {'Major':<5}")
         student = cursor.fetchone()
-        print(student) #Note: needs formatting so it doesn't just show the raw data
+
+        #Unpack student into multipel variables
+        stud_id, name, age, gender, major, phone = student
+
+        
+        #Display the relevant student information
+        print(f"{stud_id:<10} {name:<16} {phone:<15} {major:<5}")
         
 def deleteStudent(ID):
 
@@ -71,8 +95,8 @@ def showScoreName(name):
         cursor.execute("SELECT * FROM Score WHERE name = ?", (name,))
 
         #Header
-        #printPage("StudentRecord.txt")
-        print(f"{'ID':<8} {'Name':<15} {'Phone':<15} {'Major':<5}") #Note: 'Phone' and 'Major' need to be replaced with classes
+        printPage("StudentRecord.txt")
+        print(f"{'ID':<8} {'Name':<15} ") #Note: the student's classes need to be added
 
         #Displays student scores
         students = cursor.fetchall()
@@ -90,8 +114,8 @@ def showScoreID(ID):
         cursor.execute("SELECT * FROM Score WHERE id = ?", (ID,))
 
         #Header
-        #printPage("StudentRecord.txt")
-        print(f"{'ID':<8} {'Name':<15} {'Phone':<15} {'Major':<5}") #Note: 'Phone' and 'Major' need to be replaced with classes
+        printPage("StudentRecord.txt")
+        print(f"{'ID':<8} {'Name':<15} ") #Note: the student's classes need to be added
 
         #Displays student scores
         students = cursor.fetchall()
