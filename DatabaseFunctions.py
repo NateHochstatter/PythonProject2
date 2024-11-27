bfeabac0cbfb44d786f473433b87f90ea089e4a3
noahm06304 <117481568+noahm06304@users.noreply.github.com>
2024-11-27 12:54:53 -0600

    Update DatabaseFunctions.py
    Added showAllStudents, showScoreName, and showScoreID

--- DatabaseFunctions.py
+++ DatabaseFunctions.py
@@ -10,6 +10,22 @@ def addStudent(ID, name, age, gender, major, phone):
         cursor.execute("INSERT INTO Student (id, name, age, gender, major, phone) VALUES (?, ?, ?, ?, ?, ?)",
                        (ID, name, age, gender, major, phone))
 
+def showAllStudents():
+    with sqlite3.connect("school.db") as conn:
+        cursor = conn.cursor()
+
+        #Selects all students
+        cursor.execute("SELECT * FROM Student")
+
+        students = cursor.fetchall()
+
+        #Header
+        #printPage("StudentRecord.txt")
+        print(f"{'ID':<8} {'Name':<15} {'Phone':<15} {'Major':<5}")
+        for student in students:
+            print(student) #Note: needs formatting so it doesn't just show the raw data
+        print()
+
 def showStudentName(name):
     with sqlite3.connect("school.db") as conn:
         cursor = conn.cursor()
@@ -18,11 +34,11 @@ def showStudentName(name):
         cursor.execute("SELECT * FROM Student WHERE name = ?", (name,))
 
         printPage("StudentRecord.txt")
-        rows = cursor.fetchall()
+        students = cursor.fetchall()
 
-        if rows:
-            for i in rows:
-                print(i)
+        if students:
+            for student in students:
+                print(i) #Note: needs formatting so it doesn't just show the raw data
             print()
 
 def showStudentID(ID):
@@ -34,7 +50,7 @@ def showStudentID(ID):
 
         printPage("StudentRecord.txt")
         student = cursor.fetchone()
-        print(student)
+        print(student) #Note: needs formatting so it doesn't just show the raw data
         
 def deleteStudent(ID):
 
@@ -44,3 +60,41 @@ def deleteStudent(ID):
 
         #Deletes student information from the database
         cursor.execute("DELETE FROM Student WHERE id = ?", (ID,))
+
+def showScoreName(name):
+
+    #Connects to the database
+    with sqlite3.connect("school.db") as conn:
+        cursor = conn.cursor()
+
+        #Selects students based on their name
+        cursor.execute("SELECT * FROM Score WHERE name = ?", (name,))
+
+        #Header
+        #printPage("StudentRecord.txt")
+        print(f"{'ID':<8} {'Name':<15} {'Phone':<15} {'Major':<5}") #Note: 'Phone' and 'Major' need to be replaced with classes
+
+        #Displays student scores
+        students = cursor.fetchall()
+        for student in students:
+            print(student) #Note: needs formatting so it doesn't just show the raw data
+        print()
+    
+def showScoreID(ID):
+
+    #Connects to the database
+    with sqlite3.connect("school.db") as conn:
+        cursor = conn.cursor()
+
+        #Selects student based on their ID
+        cursor.execute("SELECT * FROM Score WHERE id = ?", (ID,))
+
+        #Header
+        #printPage("StudentRecord.txt")
+        print(f"{'ID':<8} {'Name':<15} {'Phone':<15} {'Major':<5}") #Note: 'Phone' and 'Major' need to be replaced with classes
+
+        #Displays student scores
+        students = cursor.fetchall()
+        for student in students:
+            print(student) #Note: needs formatting so it doesn't just show the raw data
+        print()
