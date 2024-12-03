71d5d814b8aefba2f93444f8fce19bcb60037df2
Cobble <cobblestonerules04@gmail.com>
2024-12-03 11:59:32 -0600

    Update and rename DnDDatabaseFunction to DnDDatabaseFunction.py

---
+++ DnDDatabaseFunction.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+def showAllCharacters():
+    with sqlite3.connect("Dnd.db") as conn:
+        cursor = conn.cursor()
+
+        #Selects all Characters
+        cursor.execute("SELECT * FROM Dnd")
+
+        characters = cursor.fetchall()
+
+        #Header
+        printPage("DndRecord.txt")
+        print(f"{'ID':<10} {'CharId':<5} {'Name':<16} {'Class':<15} {'Level':<5} {'Race':<5} {'Campaign':<5}")
+
+        #Iterate through every student
+        for characters in characters:
+
+            #Unpack student into multiple variables
+            stud_id, char_id, name, clas, level, race, campaign = student
+
+            #Display the relevant student information
+            print(f"{stud_id:<10} {char_id:<5} {name:<5} {clas:<5} {level:<5} {race:<5} {campaign:<5}")
+        print()
