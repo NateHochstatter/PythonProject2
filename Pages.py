794fa841398418d18cbf9af18361db3b53a42c65
noahm06304 <117481568+noahm06304@users.noreply.github.com>
2024-12-03 15:44:18 -0600

    Update Pages.py
    Added modifyStudentPrompt and modifyStudentPage

--- Pages.py
+++ Pages.py
@@ -137,6 +137,76 @@ def showStudentPage():
     except:
         return
 
+def modifyStudentPrompt():
+
+    try:
+        ID = input("Enter Student ID to modify: ")
+
+    except:
+        print("No record found")
+
+    if IDExists(ID) == True:
+        modifyStudentPage(ID)
+    else:
+        print("No record found")
+
+def modifyStudentPage(ID):
+
+    #Variable for checking if input is valid
+    validItem = False
+
+    #Age
+    while validItem == False:
+        try:
+            newAge = int(input("Enter student's age: "))
+
+            #Determine if the selected age is valid
+            if checkAge(newAge) != True:
+                #Invalid
+                print("Invalid student age")
+            else:
+                #Valid
+                validItem = True
+        except:
+            print("Invalid student age")
+
+    #Major
+    validItem = False
+    while validItem == False:
+        try:
+            newMajor = input("Enter student's major: ")
+
+            #Determine if the selected major is valid
+            if checkMaj(newMajor) != True:
+                #Invalid
+                print("Invalid student major")
+            else:
+                #Valid
+                validItem = True
+                newMajor = newMajor.upper()
+        except:
+            print("Invalid student major")
+
+    #Phone
+    validItem = False
+    while validItem != True:
+        try:
+            newPhone = input("Enter student's phone number: ")
+
+            #Determine if the selected major is valid
+            if checkPhone(newPhone) == False:
+                #Invalid
+                print("Invalid phone number")
+            else:
+                #Valid
+                validItem = True
+        except:
+            print("Invalid phone number")
+
+    modifyStudent(ID, newAge, newMajor, newPhone)
+    print("Record modified successfully!")
+
+
 def deleteStudentPage():
     try:
         printPage("Delete.txt")
